2f7b07314cc60cc600ea6a831213322295d82d98
jing zhu <you@example.com>
2022-03-10 14:01:33 +1300

    Add ListExample.

---
+++ src/list/ListExample.js
@@ -0,0 +1,22 @@
+import React, { component } from "react";
+
+class ListExample extends component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            todo: [
+                'Watch movie',
+                'Have dinner',
+                'Play game',
+            ]
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
